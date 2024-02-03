c915d62aa7b0ac854805d4c3a3b29c92db4e6f29
FuwwyCodingCOwO <144972258+FuwwyCodingCOwO@users.noreply.github.com>
2024-02-02 19:06:01 -0600

    Create C++ examples.cpp

---
+++ Input Output example/C++ examples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int variable_name;
+
+	std::cout << "enter a vlue: ";
+
+	std::cin >> variable_name;
+
+	std::cout << "variable = " << variable_name << std::endl;
+
+	return 0;
+}
